75280887800bb04bf6d2c0ec1fdbfd9cc235dcc0
Hyunwoosong <h.song5@student.vu.nl>
2024-01-29 18:18:35 +0100

    update evaluation of Euclidian distances

--- executables/evaluation.py
+++ executables/evaluation.py
@@ -2,35 +2,31 @@ import math
 import random
 import pathlib
 import re
+from typing import List
 
 
 from typing import Optional, List
 
 
-def distance(file1: List[int], file2: List[int]) -> int:
+def distance_round(list1: List[int], list2: List[int]) -> int:
     '''
         it chechs Euclidian similarity of the data set.
     '''
-    if len(file1) != len(file2):
+    if len(list1) != len(list2):
         raise ValueError('Those have different number of elements!')
             
+    return math.sqrt(sum((int(a) - int(b)) ** 2 for a, b in zip(list1[:-1], list2[:-1])))
     
-    if list1[-1] == 1 and list2[-1] == 1:
-        return math.sqrt(sum((a - b) ** 2 for a, b in zip(list1[:-1], list2[:-1])))
-    
-    else:
-        return -1
-    
-def extract_numbers_from_file(file_path):
+def extract_moves_from_file(file_path):
     '''
-        It cleans the output files with spliting into the numbers of digits(174).
+        It cleans the output files with spliting into the moves of digits(174).
         Each sublist in the list represent the game record of each round.
     '''
     try:
         with open(file_path, 'r') as file:
             content = file.read()
-            numbers = re.findall(r'\b\d+\b', content)
-            divided_lists = [numbers[i * 174:(i + 1) * 174] for i in range(int(len(numbers)/174))]
+            moves = re.findall(r'\b\d+\b', content)
+            divided_lists = [moves[i * 174:(i + 1) * 174] for i in range(int(len(moves)/174))]
             return divided_lists
     except FileNotFoundError:
         print("File not found.")
@@ -38,14 +34,28 @@ def extract_numbers_from_file(file_path):
     except Exception as e:
         print(f"An error occurred: {e}")
         return []
-    
+
+def distance_game(list1: List[List[int]], list2: List[List[int]]) -> int:
+    '''
+        It caculates a Euclidian distance for whole rounds(game).
+        return: average distance between the moves.
+    '''
+    sum = 0
+    count = 0
+    for i in range(len(list1)):
+        if (int(list1[i][-1]) == 1) and (int(list2[i][-1]) == 1):
+            sum += distance_round(list1[i],list2[i])
+            count += 1
+    print(f'sum of distance: {sum} and number of comparation: {count}')
+    return round(sum/count,3)
 
 ObserverBot_move_file_path = '/Users/song/Projects/schnapsen/Observer_replay_memories/random_random_10k_games_100_to_compare-1.txt'
 RdeepBot_move_file_path = '/Users/song/Projects/schnapsen/Observer_replay_memories/random_random_10k_games_100.txt'
 
-numbers_list1 = extract_numbers_from_file(ObserverBot_move_file_path)
-numbers_list2 = extract_numbers_from_file(RdeepBot_move_file_path)
+moves_list1 = extract_moves_from_file(ObserverBot_move_file_path)
+moves_list2 = extract_moves_from_file(RdeepBot_move_file_path)
 
-print(len(numbers_list2))
-# print(len(numbers_list1))
+# print(moves_list2[0])
+print(distance_game(moves_list1,moves_list2))
+# print(len(moves_list1))
 
